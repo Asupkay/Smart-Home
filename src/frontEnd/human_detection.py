f77b10db114cf8b14d210767c80d22607f0f5510
Sanjeev Rajasekaran <sanjeev95@rocketmail.com>
2018-09-27 13:11:34 -0400

    Update human_detection.py
    Added comments

--- src/frontEnd/human_detection.py
+++ src/frontEnd/human_detection.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import time
 
 # from medium article
@@ -35,26 +34,22 @@ while cap.isOpened():
             frame = cv2.resize(frame, (960, 540))  # Downscale to improve frame rate. Can adjust this value as needed
             #Interestingly, it gives an error if I downscale it by half i.e 480x270 Try it out
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)  # HOG needs a grayscale image. Convert frame from rgb to grayscale
-            rects, weights = hog.detectMultiScale(gray_frame, ) # actual detection of humans using HOG
-
+            rects, confidence = hog.detectMultiScale(gray_frame, ) # actual detection of humans using HOG
 
             # draw rectangles
             for i, (x, y, w, h) in enumerate(rects):
-
-                if weights[i] < 0.8:
+                if confidence[i] < 0.8: #if the system is really confident, then draw the rectangle or just go to next iteration
                     continue
                 else:
-                    print(("Confidence value is {}".format(weights)))
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    print(("Confidence value is {}".format(confidence)))
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) # draw rectangle
 
                     # cropping box from frame
                     cropImg = frame[y: y + h, x: x + w]
-                    cv2.imshow('cropImg', cropImg)
+                    cv2.imshow('cropImg', cropImg) #show cropped image in window
 
             # Display the resulting frame
             cv2.imshow('Frame', frame)
-
-
 
             # Press Q on keyboard to exit
             if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -67,12 +62,10 @@ while cap.isOpened():
     else:
         continue
 
-
-
 # When everything done, release the video capture object
 cap.release()
 
 # Closes all the frames
 cv2.destroyAllWindows()
 
-print("Execution time ---> %s seconds " % ((time.time() - start_time)))
+print("Execution time ---> %s seconds " % ((time.time() - start_time))) #print code execution time

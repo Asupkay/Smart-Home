5303f40af1a6a3bb2dec1e27fc03ab3f00bc7660
kylerozanitis <kyle.rozanitis@gmail.com>
2018-11-14 18:20:24 -0500

    added working and scrubbed sensor.py

--- src/frontEnd/sensor.py
+++ src/frontEnd/sensor.py
@@ -17,21 +17,24 @@ try:
             last_recorded_capture = datetime.datetime.now()
             print(last_recorded_capture)
             # cm.take_picture()
-            subprocess.Popen("ssh pi@155.246.219.87 'python /home/pi/Desktop/camera.py'", shell=True)
-            print("Motion Detected - Picture Taken")
+            subprocess.Popen("ssh pi@ipaddress python /home/pi/Desktop/camera.py", shell=True)
+            print("\n Motion Detected - Picture Taken")
             print(datetime.datetime.now())
             delta_value = (datetime.datetime.now() - last_recorded_capture)
             print(delta_value.seconds)
         else:
-            print(datetime.datetime.now())
-            print("No motion")
-            delta_value = (datetime.datetime.now() - last_recorded_capture)
-            print(delta_value.seconds)
-            if delta_value.seconds > 3:
-                print("Camera Closed")
+            try:
+                print(datetime.datetime.now())
+                print("No motion")
+                delta_value = (datetime.datetime.now() - last_recorded_capture)
+                print(delta_value.seconds)
+                if delta_value.seconds > 3:
+                    print("Camera Closed")
                 # cm.close_camera()
                 # cm= None
-        time.sleep(0.1)
+            except:
+                print("Error")
+        time.sleep(3)
 
 except Exception as e:
     print(e)
